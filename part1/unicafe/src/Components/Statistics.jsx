d58958484fc6799c981973ffb8b7acf83430cf2e
Igor Burykin <iburykin071@gmail.com>
2023-11-08 13:09:12 +0200

    1.9: unicafe step4

--- part1/unicafe/src/Components/Statistics.jsx
+++ part1/unicafe/src/Components/Statistics.jsx
@@ -1,24 +1,28 @@
 import StatField from "./StatField"
 
 const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <>
-      <h1>Statistics</h1>
+  if (good > 0 || neutral > 0 || bad > 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
 
-      <StatField text="Good" num={good} />
-      <StatField text="Neutral" num={neutral} />
-      <StatField text="Bad" num={bad} />
-      <StatField text="All" num={good + neutral + bad} />
-      <StatField
-        text="Average"
-        num={((good - bad) / (good + neutral + bad)).toFixed(2)}
-      />
-      <StatField
-        text="Positive"
-        num={((good * 100) / (good + neutral + bad)).toFixed(2) + "%"}
-      />
-    </>
-  )
+        <StatField text="Good" num={good} />
+        <StatField text="Neutral" num={neutral} />
+        <StatField text="Bad" num={bad} />
+        <StatField text="All" num={good + neutral + bad} />
+        <StatField
+          text="Average"
+          num={((good - bad) / (good + neutral + bad)).toFixed(2)}
+        />
+        <StatField
+          text="Positive"
+          num={((good * 100) / (good + neutral + bad)).toFixed(2) + "%"}
+        />
+      </>
+    )
+  } else {
+    return <p>No feedback given</p>
+  }
 }
 
 export default Statistics
